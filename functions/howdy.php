85b5fd245ab7700bd371efa8925e3c62670d9f5c
Jason Jersey <icryptic@users.noreply.github.com>
2014-01-22 23:20:32 -0700

    Update howdy.php
    Temporary patch

--- functions/howdy.php
+++ functions/howdy.php
@@ -9,8 +9,14 @@ function change_howdy($translated, $text, $domain) {
         return $translated;
 
     if (false !== strpos($translated, 'Howdy'))
-        $howdy_text = get_option("wpqorefunc_howdy_text");
-        return str_replace('Howdy', __( $howdy_text, 'wp-qore' ), $translated);
+
+        //Want to help fix this? Check the issue trac: 
+        //https://github.com/icryptic/wp-qore/issues/38
+        //
+        //$howdy_text = get_option("wpqorefunc_howdy_text");
+        //return str_replace('Howdy', __( $howdy_text, 'wp-qore' ), $translated);
+        
+        return str_replace('Howdy', get_option("wpqorefunc_howdy_text"), $translated);
 
     return $translated;
 }
